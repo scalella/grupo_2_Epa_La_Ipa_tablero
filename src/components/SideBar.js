bade20d2a68f04a31dbe97bd54bfd695e28092f6
AkiraJit <akirajit@gmail.com>
2022-08-21 19:05:53 -0300

    iniciando el tablero en 3, 2, 1 ...

---
+++ src/components/SideBar.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import image from '../assets/images/solo logo.png';
+import ContentWrapper from './ContentWrapper';
+import GenresInDb from './GenresInDb';
+import LastMovieInDb from './LastMovieInDb';
+import ContentRowMovies from './ContentRowMovies';
+import NotFound from './NotFound';
+import Chart from './Chart'
+import {Link, Route, Switch} from 'react-router-dom';
+import './SideBar.css'
+
+function SideBar(){
+    return(
+        <React.Fragment>
+            {/*<!-- Sidebar -->*/}
+            <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                {/*<!-- Sidebar - Brand -->*/}
+                <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                    <div className="sidebar-brand-icon">
+                        <img className="w-50" src={image} alt="Epa La Ipa"/>
+                    </div>
+                </a>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider my-0"/>
+
+                {/*<!-- Nav Item - Dashboard -->*/}
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                        <span>EPA! El Dashboard 👽 </span></Link>
+                </li>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider"/>
+
+                {/*<!-- Heading -->*/}
+                <div className="sidebar-heading">Menú</div>
+
+                {/*<!-- Nav Item - Pages -->*/}
+                <li className="nav-item">
+                <Link className="nav-link" to="/GenresInDb">
+                        <i className="fas fa-fw fa-folder"></i>
+                        <span>Estilos de cerveza</span>
+                    </Link>
+                </li>
+
+                {/*<!-- Nav Item - Charts -->*/}
+                <li className="nav-item">
+                    <Link className="nav-link" to="/LastMovieInDb">
+                        <i className="fas fa-fw fa-chart-area"></i>
+                        <span>Detalle último producto</span></Link>
+                </li>
+
+                {/*<!-- Nav Item - Tables -->*/}
+                <li className="nav-item nav-link">
+                <Link className="nav-link" to="/chart">
+                        <i className="fas fa-fw fa-table"></i>
+                        <span>Listado de todas las birras</span></Link>
+                </li>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider d-none d-md-block"/>
+            </ul>
+            {/*<!-- End of Sidebar -->*/}
+
+            {/*<!-- Microdesafio 1 -->*/}
+            {/*<!--<Route exact path="/">
+                <ContentWrapper />
+            </Route>
+            <Route path="/GenresInDb">
+                <GenresInDb />
+            </Route>
+            <Route path="/LastMovieInDb">
+                <LastMovieInDb />
+            </Route>
+            <Route path="/ContentRowMovies">
+                <ContentRowMovies />
+            </Route>*/}
+            {/*<!-- End Microdesafio 1 -->*/}
+
+            {/*<!-- End Microdesafio 2 -->*/}
+            <Switch>
+                <Route exact path="/">
+                    <ContentWrapper />
+                </Route>
+                <Route path="/GenresInDb">
+                    <GenresInDb />
+                </Route>
+                <Route path="/LastMovieInDb">
+                    <LastMovieInDb />
+                </Route>
+                <Route path="/ContentRowMovies">
+                    <ContentRowMovies />
+                </Route>
+                <Route path="/chart">
+                    <Chart />
+                </Route>
+                <Route component={NotFound} />
+            </Switch>
+            {/*<!-- End Microdesafio 2 -->*/}
+        </React.Fragment>
+    )
+}
+export default SideBar;
